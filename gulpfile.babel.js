a873c4e1f2988cc5860cab3e019248c553b97156
myamyang_works <dlekddl43@gmail.com>
2022-12-13 18:36:56 +0900

    4.0 Gulp SASS

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,16 +3,25 @@ import gpug from "gulp-pug";
 import del from "del";
 import ws from "gulp-webserver";
 import image from "gulp-image";
+import gulpSass from "gulp-sass";
+import nodeSass from "sass";
+
+const sass = gulpSass(nodeSass);
 
 const routes = {
   pug: {
-    watch: "src/**/*.pug", // monitering
+    watch: "src/**/*.pug", // Monitering
     src: "src/*.pug", // Comfile
     dest: "build",
   },
   img: {
     src: "src/img/*",
     dest: "build/img",
+  },
+  scss: {
+    watch: "src/scss/**/*.scss",
+    src: "src/scss/style.scss",
+    dest: "build/css",
   },
 };
 
@@ -25,9 +34,17 @@ const clean = async () => del(["build"]);
 const webserver = () =>
   gulp.src("build").pipe(ws({ livereload: true, open: true }));
 
+const styles = () =>
+  gulp
+    .src(routes.scss.src)
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest(routes.scss.dest));
+
+// Monitering
 const watch = () => {
   gulp.watch(routes.pug.watch, pug);
   gulp.watch(routes.img.src, img);
+  gulp.watch(routes.scss.watch, styles);
 };
 
 const img = () =>
@@ -35,7 +52,7 @@ const img = () =>
 
 const prepare = gulp.series([clean, img]);
 
-const assets = gulp.series([pug]);
+const assets = gulp.series([pug, styles]);
 
 const postDev = gulp.parallel([webserver, watch]);
 
