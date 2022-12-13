9cefc3fa871dab7b3d4c395793a3684992f5fb4c
myamyang_works <dlekddl43@gmail.com>
2022-12-13 19:42:15 +0900

    6.0 Deploying to GH Pages

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -9,6 +9,7 @@ import autoprefixer from "gulp-autoprefixer";
 import miniCSS from "gulp-csso";
 import bro from "gulp-bro";
 import babelify from "babelify";
+import ghPages from "gulp-gh-pages";
 
 const sass = gulpSass(nodeSass);
 
@@ -38,7 +39,7 @@ const pug = () =>
   gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
 
 // The build directory delete
-const clean = async () => del(["build"]);
+const clean = async () => del(["build/", ".publish"]);
 
 const webserver = () =>
   gulp.src("build").pipe(ws({ livereload: true, open: true }));
@@ -67,6 +68,8 @@ const js = () =>
     )
     .pipe(gulp.dest(routes.js.dest));
 
+const gh = () => gulp.src("build/**/*").pipe(ghPages());
+
 // Monitering
 const watch = () => {
   gulp.watch(routes.pug.watch, pug);
@@ -79,6 +82,8 @@ const prepare = gulp.series([clean, img]);
 
 const assets = gulp.series([pug, styles, js]);
 
-const postDev = gulp.parallel([webserver, watch]);
+const live = gulp.parallel([webserver, watch]);
 
-export const dev = gulp.series([prepare, assets, postDev]);
+export const build = gulp.series([prepare, assets]);
+export const dev = gulp.series([prepare, live]);
+export const deploy = gulp.series([build, gh, clean]);
