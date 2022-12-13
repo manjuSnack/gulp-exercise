bfd9f99814255f1a80d92bd7a8bfbfcd798e43c4
myamyang_works <dlekddl43@gmail.com>
2022-12-13 17:54:19 +0900

    3.0 Image Optimization

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,12 +2,17 @@ import gulp from "gulp";
 import gpug from "gulp-pug";
 import del from "del";
 import ws from "gulp-webserver";
+import image from "gulp-image";
 
 const routes = {
   pug: {
     watch: "src/**/*.pug", // monitering
     src: "src/*.pug", // Comfile
     dest: "build",
+  },
+  img: {
+    src: "src/img/*",
+    dest: "build/img",
   },
 };
 
@@ -22,9 +27,13 @@ const webserver = () =>
 
 const watch = () => {
   gulp.watch(routes.pug.watch, pug);
+  gulp.watch(routes.img.src, img);
 };
 
-const prepare = gulp.series([clean]);
+const img = () =>
+  gulp.src(routes.img.src).pipe(image()).pipe(gulp.dest(routes.img.dest));
+
+const prepare = gulp.series([clean, img]);
 
 const assets = gulp.series([pug]);
 
